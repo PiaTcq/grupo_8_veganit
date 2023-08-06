dcd621ea7ac2d1717a985677ec0589f324313913
MatiasZarate <matiasvargas3112@gmail.com>
2023-08-06 20:19:27 -0300

    imagenperfil

--- controllers/registercontroller.js
+++ controllers/registercontroller.js
@@ -19,13 +19,16 @@ const controlador = {
                 newCuenta = {
                 id : Date.now(),
                 nombre: req.body.nombre,
-                contraseña: bcrypt.hashSync(req.body.contraseña, 10),
-                confirmar: req.body.confirmar,
+                /*contraseña: bcrypt.hashSync(req.body.contraseña, 10),*/
+                /*contraseña: req.body.contraseña,*/
+                password: bcrypt.hashSync(req.body.password, 10),
+                /*password: req.body.password,*/
                 email: req.body.email,
                 pais: req.body.pais,
                 localidad: req.body.localidad,
                 direccion: req.body.direccion,
-                genero: req.body.genero
+                genero: req.body.genero,
+                imagen: req.file.filename
             }
             cuentas.push(newCuenta);
             const cuentasJson = JSON.stringify(cuentas, null, ' ')
