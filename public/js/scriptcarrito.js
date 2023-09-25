97365d3c2f464ccbd3cda4bf825ba5c0c9f7bbf3
MatiasZarate <matiasvargas3112@gmail.com>
2023-09-25 16:44:39 -0300

    carritoparte3

--- public/js/scriptcarrito.js
+++ public/js/scriptcarrito.js
@@ -1,74 +1,41 @@
-//window.addEventListener("DOMContentLoaded", function(){
+// Lista de todos los contenedores de los productos 
+let productsList = document.querySelectorAll(".plato-caja");
 
- 
-    
+// Variable de arreglo de productos (los productos seleccionados para el carrito)
+let allProducts = [];
 
+// Función para mostrar HTML
+let showHTML = () => {
+  let rowProduct = document.querySelector(".rowProduct");
+  rowProduct.innerHTML = '';
 
-    
+  allProducts.forEach(product => {
+    let containerProduct = document.createElement("div");
+    containerProduct.classList.add("caja");
 
-    
-    //let rowProduct= document.getElementById("caja")
+    containerProduct.innerHTML = `
+      <h1>${product.title}</h1>
+      <h2>${product.price}</h2>
+      <h3>cantidad: ${product.quantity}</h3>
+    `;
 
+    rowProduct.appendChild(containerProduct);
+  })
+};
 
-    //lista de todos los contenedores de los productos 
-    let productsList = document.querySelector(".plato-caja")
+// Agrega el event listener al contenedor principal que envuelve todos los elementos .plato-caja
+let productsListContainer = document.querySelector(".container"); // Asegúrate de que este selector coincida con tu estructura
+productsListContainer.addEventListener("click", e => {
+  if (e.target.classList.contains("añadir")) {
+    let product = e.target.parentElement.parentElement;
 
-    //variable de arreglos de productos(los productos que selecciono para el carrito)
-    let allProducts = [];
+    let infoProduct = {
+      quantity: 1,
+      title: product.querySelector(".nombrePlato").textContent,
+      price: product.querySelector(".precio").textContent
+    };
 
-    //función para mostrar html
-   
-
-    productsList.addEventListener("click", e=>{
-       if(e.target.classList.contains("añadir")){
-        let product = e.target.parentElement.parentElement
-       
-
-        let infoProduct = {
-            quantity: 1,
-            title: product.querySelector("h4").textContent,
-            price: product.querySelector("h5").textContent/*,
-            image: product.querySelector("h4").textContent*/
-        }
-
-        
-
-        let showHTML = ()=>{
-          let rowProduct= document.querySelector(".rowProduct")
-
-          rowProduct.innerHTML='';
-          
-        allProducts.forEach(product => {
-            let containerProduct = document.createElement("div")
-            containerProduct.classList.add("caja");
-    
-            containerProduct.innerHTML = `
-            
-              <h4>Aca irá la imagen xd</h4>
-              <h1>${product.title}</h1>
-              <h2>${product.price}</h2>
-              <h3>cantidad: ${product.quantity}</h3>
-            
-          `
-           //console.log(rowProduct)
-    
-          rowProduct.append(containerProduct)
-    
-        })
-    }
-        //console.log(infoProduct)
-        allProducts = [...allProducts, infoProduct]
-        showHTML();
-
-        
-       
-
-       }
-       //console.log(allProducts)
-
-    })
-
-    
-
-
-//})
+    allProducts = [...allProducts, infoProduct];
+    showHTML();
+  }
+});
