0b3aef8ad41d484bcadd585849b4b29ea9cb2df1
MatiasZarate <matiasvargas3112@gmail.com>
2023-09-25 15:41:21 -0300

    carritoparte2

--- public/js/scriptcarrito.js
+++ public/js/scriptcarrito.js
@@ -1,5 +1,15 @@
-window.addEventListener("load", function(){
-    let rowProduct= document.querySelector(".row-product")
+//window.addEventListener("DOMContentLoaded", function(){
+
+ 
+    
+
+
+    
+
+    
+    //let rowProduct= document.getElementById("caja")
+
+
     //lista de todos los contenedores de los productos 
     let productsList = document.querySelector(".plato-caja")
 
@@ -7,23 +17,7 @@ window.addEventListener("load", function(){
     let allProducts = [];
 
     //función para mostrar html
-   let showHTML = ()=>{
-    allProducts.forEach(product => {
-        let containerProduct = document.createElement("div")
-        containerProduct.classList.add("caja");
-
-        containerProduct.innerHTML = `
-        <div class="relleno">
-          <h4>Aca irá la imagen xd</h4>
-          <h1>${product.title}</h1>
-          <h2>${product.price}</h2>
-          <h3>cantidad: ${product.quantity}</h3>
-        </div>
-      `
-
-      rowProduct.append(containerProduct)
-    })
-}
+   
 
     productsList.addEventListener("click", e=>{
        if(e.target.classList.contains("añadir")){
@@ -36,16 +30,45 @@ window.addEventListener("load", function(){
             price: product.querySelector("h5").textContent/*,
             image: product.querySelector("h4").textContent*/
         }
+
+        
+
+        let showHTML = ()=>{
+          let rowProduct= document.querySelector(".rowProduct")
+
+          rowProduct.innerHTML='';
+          
+        allProducts.forEach(product => {
+            let containerProduct = document.createElement("div")
+            containerProduct.classList.add("caja");
+    
+            containerProduct.innerHTML = `
+            
+              <h4>Aca irá la imagen xd</h4>
+              <h1>${product.title}</h1>
+              <h2>${product.price}</h2>
+              <h3>cantidad: ${product.quantity}</h3>
+            
+          `
+           //console.log(rowProduct)
+    
+          rowProduct.append(containerProduct)
+    
+        })
+    }
         //console.log(infoProduct)
         allProducts = [...allProducts, infoProduct]
         showHTML();
-       }
-       console.log(allProducts)
 
+        
        
+
+       }
+       //console.log(allProducts)
+
     })
 
     
 
 
-})
+//})
