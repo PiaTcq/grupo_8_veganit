4e89ef07c811d01b18c4e6d373d4567509712ad9
MatiasZarate <matiasvargas3112@gmail.com>
2023-09-26 11:47:25 -0300

    carritoparte4creo

--- public/js/scriptcarrito.js
+++ public/js/scriptcarrito.js
@@ -1,26 +1,41 @@
 // Lista de todos los contenedores de los productos 
 let productsList = document.querySelectorAll(".plato-caja");
+let rowProduct = document.querySelector(".rowProduct");
 
 // Variable de arreglo de productos (los productos seleccionados para el carrito)
 let allProducts = [];
+let valorTotal = document.querySelector(".total")
 
 // Función para mostrar HTML
 let showHTML = () => {
-  let rowProduct = document.querySelector(".rowProduct");
+  //let rowProduct = document.querySelector(".rowProduct");
   rowProduct.innerHTML = '';
+
+  let total= 0;
 
   allProducts.forEach(product => {
     let containerProduct = document.createElement("div");
     containerProduct.classList.add("caja");
 
     containerProduct.innerHTML = `
-      <h1>${product.title}</h1>
+      <h1 class="p">${product.title}</h1>
       <h2>${product.price}</h2>
       <h3>cantidad: ${product.quantity}</h3>
+      <h4 class="borrar"> borrar producto </h4>
     `;
+   /* containerProduct.innerHTML = `
+  <h1 class="nombrePlato">${product.title}</h1>
+  <h2>${product.price}</h2>
+  <h3>cantidad: ${product.quantity}</h3>
+  <h4 class="borrar">borrar producto</h4>
+`;*/
+
 
     rowProduct.appendChild(containerProduct);
+
+    total = total + parseInt(product.quantity * product.price.slice(1))
   })
+    valorTotal.innerText = `$${total}`
 };
 
 // Agrega el event listener al contenedor principal que envuelve todos los elementos .plato-caja
@@ -28,6 +43,7 @@ let productsListContainer = document.querySelector(".container"); // Asegúrate 
 productsListContainer.addEventListener("click", e => {
   if (e.target.classList.contains("añadir")) {
     let product = e.target.parentElement.parentElement;
+    
 
     let infoProduct = {
       quantity: 1,
@@ -54,4 +70,34 @@ productsListContainer.addEventListener("click", e => {
     
     showHTML();
   }
+
+  
 });
+
+rowProduct.addEventListener("click", (e) => {
+  if (e.target.classList.contains("borrar")) {
+    const product = e.target.parentElement;
+    const title = product.querySelector(".p").textContent;
+
+    
+    const productIndex = allProducts.findIndex((product) => product.title === title);
+
+    if (productIndex !== -1) {
+      
+      allProducts[productIndex].quantity--;
+
+      
+      if (allProducts[productIndex].quantity === 0) {
+        allProducts.splice(productIndex, 1);
+      }
+
+      showHTML();
+    }
+  }
+});
+
+
+/*rowProduct.addEventListener("press", (e) => {
+  if (e.target.classList.contains("comprar")){
+    console.log("gracias por su compa :)")
+  }})*/
