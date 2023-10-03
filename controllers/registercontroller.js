34b460821e61e24467f2c7754f7a80d0133493bf
MatiasZarate <matiasvargas3112@gmail.com>
2023-10-03 12:14:22 -0300

    intentandosolucionarimagenes

--- controllers/registercontroller.js
+++ controllers/registercontroller.js
@@ -8,12 +8,13 @@ let db = require("../database/models")
 const { v4: uuidv4 } = require('uuid');
 const uuid = uuidv4();
 const cloudinary = require("cloudinary").v2;
-/*const streamifier = require("streamifier")*/
+const streamifier = require("streamifier")
  
 cloudinary.config({
     cloud_name: "dduyxqrqt",
     api_key: "867588739315874",
-    api_secret: "meBOrwZzq5JG1CZJmut8pqVFtT0"
+    api_secret: "meBOrwZzq5JG1CZJmut8pqVFtT0",
+    debug: true
 });
 
 const controlador = {
@@ -28,15 +29,55 @@ const controlador = {
 
         res.render("users/register" );
     },
-    postRegister: (req, res) =>{
+    postRegister: async (req, res) => {
+      let errors = validationResult(req);
+      
+      /*console.log(req.file)*/
+    
+      if (errors.isEmpty()) {
+        let hashedPassword = bcrypt.hashSync(req.body.password, 10);
+        const customFilename = `${Date.now()}`;
+    
+        try {
+          
+          const cloudinaryUpload = await cloudinary.uploader.upload(req.file.path, {
+            public_id: customFilename,
+            overwrite: true
+          });
+    
+          // Crea el usuario en la base de datos con la URL de la imagen de Cloudinary
+          const newUser = await db.usuario.create({
+            id: Date.now(),
+            nombre: req.body.nombre,
+            contraseña: hashedPassword,
+            email: req.body.email,
+            pais: req.body.pais,
+            localidad: req.body.localidad,
+            direccion: req.body.direccion,
+            genero: req.body.genero,
+            imagen: cloudinaryUpload.secure_url
+          });
+          /*streamifier.createReadStream(imageBuffer).pipe(newUser)*/
+          res.redirect("/");
+        } catch (error) {
+          console.error("Error:", error);
+          res.status(500).send("Error al cargar la imagen o crear el usuario en la base de datos");
+        }
+      } else {
+        res.render("users/register", { errors: errors.array() });
+      }
+      
+    },
+    /*postRegister: (req, res) =>{
         let errors = validationResult(req);
+        let imageBuffer = req.file.buffer;
         
 
         if (errors.isEmpty()) {
             let hashedPassword = bcrypt.hashSync(req.body.password, 10);
-            const customFilename = `${Date.now()}`;
+            const customFilename = `${Date.now()}.jpg`;
 
-            const cloudinaryUpload =  cloudinary.uploader.upload(req.file.path, {
+            const cloudinaryUpload = cloudinary.uploader.upload(req.file.path, {
                 public_id: customFilename,
                 overwrite: true
               });
@@ -50,13 +91,15 @@ const controlador = {
                 localidad: req.body.localidad,
                 direccion: req.body.direccion,
                 genero: req.body.genero,
-                imagen: customFilename /*req.file.filename*/ /*cloudinaryUpload.secure_url*/ /*customFile*/
+                imagen: customFilename 
             }) 
             res.redirect("/");
+          
         } else {
             res.render("users/register", { errors: errors.array() });
         }
-    },
+        
+    },*/
         editar: async (req, res) => {
                 try {
                   const cuentita = await db.usuario.findByPk(req.params.id);
